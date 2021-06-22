e932c5e42076894403b7e3d54c255ef0ccb3a580
Zackary Santana <zackzackyack@gmail.com>
2021-06-21 22:47:43 -0400

    Reformated backend

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 const app = require("express")();
 
-app.use(function (req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-
 const sql = require("mysql2");
 
 const database_name = process.env.MYSQL_DATABASE;
